fae3fbba9e2466026bbcb3c256b80075342c2704
Jayanth Kumar <jknani111@gmail.com>
2021-01-29 15:13:31 +0530

    socket ports

--- app.js
+++ app.js
@@ -27,46 +27,97 @@ const io = require('socket.io')(http,{
     }
   })
 
+let lengthOfDoc=3000
+let codesMapper={} //{ '0': [ '1', '' ], '1': [ '2', '4', '' ] }
+let keywordsMapper={} 
+let leftMenuCodesMapper={} //{ '1': 'a', '2': 'b' }  
+
+const codes=({num,value})=>{
+  codesMapper={...codesMapper,[num]:value?.split(';')}
+  const filteredArray = Object.keys(leftMenuCodesMapper).filter(item => value?.split(';').includes(item));
+
+  console.log("left codes",(Object.keys(leftMenuCodesMapper)))
+  console.log("right codes",value?.split(';'))
+  console.log("filtered Array",filteredArray)
+  return
+}
+const keywords=({num,value})=>{
+  keywordsMapper={...keywordsMapper,[num]:value}
+  return
+}
+const leftmenuCodes=({i,value})=>{
+  leftMenuCodesMapper={...leftMenuCodesMapper,[i]:value}
+  console.log(leftMenuCodesMapper)
+  return
+}
 
 io.on('connection', socket => {
     socket.on('input-box', ({num,value}) => {
-        console.log( {num,value})
+      codes({num,value})
       io.emit('input-box', {num,value})
     })
     socket.on('keywords', ({num,value}) => {
-        console.log( {num,value})
+      keywords({num,value})
       io.emit('keywords', {num,value})
     })
+    socket.on('left-menu-codes', ({i,value}) => {
+      leftmenuCodes({i,value})
+      io.emit('left-menu-codes', {i,value})
+    })
+    socket.on('left-menu-state', (values) => {
+      console.log((values))
+      io.emit('left-menu-state', {values})
+    })
 })
-  
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 http.listen(4000, function() {
   console.log('http :== listening on port 4000')
 })
 
 //mongoose connect and set plugins
-mongoose.connect(process.env.DB_URL,
-    { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-     })
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+// mongoose.connect(process.env.DB_URL,
+//     { 
+//         useNewUrlParser: true, 
+//         useUnifiedTopology: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false
+//      })
+//     .then(() => console.log('MongoDB Connected'))
+//     .catch(err => console.log(err));
 
-//Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+// //Passport middleware
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-//routes
-app.use('/', require('./routes/project'));
-app.use('/', require('./routes/users'));
-app.get('/', authenticateUser, (req, res) =>{
-    res.status(200).send({ message:"Welcome at Survey Buddy",user:req.user});
-});
+// //routes
+// app.use('/', require('./routes/project'));
+// app.use('/', require('./routes/users'));
+// app.get('/', authenticateUser, (req, res) =>{
+//     res.status(200).send({ message:"Welcome at Survey Buddy",user:req.user});
+// });
 
-// define port for app
-const port = process.env.PORT || 5000;
+// // define port for app
+// const port = process.env.PORT || 5000;
 
 //config listen
 // app.listen(port, () => console.log(`server is running at port: ${port}`));
